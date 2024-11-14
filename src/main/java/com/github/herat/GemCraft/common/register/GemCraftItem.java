bd9b517816a3e8f0dd7cdfe3a70c4871e1a3c543
10565552046 <1056552046@qq.com>
2024-11-14 22:54:17 +0800

    add Items InBulk

--- src/main/java/com/github/herat/GemCraft/common/register/GemCraftItem.java
+++ src/main/java/com/github/herat/GemCraft/common/register/GemCraftItem.java
@@ -8,19 +8,29 @@ import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.SwordItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class GemCraftItem {
+
+    public static List<Supplier<? extends Item>> allItem = new ArrayList<>();
+
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM , GemCraft.MODID);
-    public static final Supplier<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties()));
-    public static final Supplier<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
-    public static final Supplier<Item> RUBY_SWORD = ITEMS.register("ruby_sword", () -> new SwordItem(GemCraftItemTiers.RUBY,new Item.Properties().attributes(SwordItem.createAttributes(GemCraftItemTiers.RUBY, 3, -2.2F))));
-    public static final Supplier<Item> SAPPHIRE_SWORD = ITEMS.register("sapphire_sword", () -> new SwordItem(GemCraftItemTiers.SAPPHIRE,new Item.Properties().attributes(SwordItem.createAttributes(GemCraftItemTiers.SAPPHIRE, 1, -1.4F))));
-    public static final Supplier<Item> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(GemCraftItemTiers.RUBY,new Item.Properties().attributes(PickaxeItem.createAttributes(GemCraftItemTiers.RUBY, 1, -2.8F))));
-    public static final Supplier<Item> SAPPHIRE_PICKAXE = ITEMS.register("sapphire_pickaxe", () -> new PickaxeItem(GemCraftItemTiers.SAPPHIRE,new Item.Properties().attributes(PickaxeItem.createAttributes(GemCraftItemTiers.RUBY, 2, -2.4F))));
+    public static final Supplier<Item> RUBY = register("ruby", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> SAPPHIRE = register("sapphire", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> RUBY_SWORD = register("ruby_sword", () -> new SwordItem(GemCraftItemTiers.RUBY,new Item.Properties().attributes(SwordItem.createAttributes(GemCraftItemTiers.RUBY, 3, -2.2F))));
+    public static final Supplier<Item> SAPPHIRE_SWORD = register("sapphire_sword", () -> new SwordItem(GemCraftItemTiers.SAPPHIRE,new Item.Properties().attributes(SwordItem.createAttributes(GemCraftItemTiers.SAPPHIRE, 1, -1.4F))));
+    public static final Supplier<Item> RUBY_PICKAXE = register("ruby_pickaxe", () -> new PickaxeItem(GemCraftItemTiers.RUBY,new Item.Properties().attributes(PickaxeItem.createAttributes(GemCraftItemTiers.RUBY, 1, -2.8F))));
+    public static final Supplier<Item> SAPPHIRE_PICKAXE = register("sapphire_pickaxe", () -> new PickaxeItem(GemCraftItemTiers.SAPPHIRE,new Item.Properties().attributes(PickaxeItem.createAttributes(GemCraftItemTiers.RUBY, 2, -2.4F))));
 
-
+    private static <ITEM extends Item> @NotNull Supplier<ITEM> register(String registerName, Supplier<? extends ITEM> sup) {
+        Supplier<ITEM> item = ITEMS.register(registerName, sup);
+        allItem.add(item);
+        return item;
+    }
 
 
 
