0c02d28522fe580958b2b981b3e2e2a0251a1efe
herat <18272892499@163.com>
2024-11-17 21:58:45 +0800

    add AT

---
+++ src/main/java/com/github/herat/GemCraft/common/item/GCArmorMmetiral/ArmorMetrial.java
@@ -0,0 +1,12 @@
+package com.github.herat.GemCraft.common.item.GCArmorMmetiral;
+
+import net.minecraft.core.Holder;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorMaterials;
+
+import java.lang.reflect.Method;
+import java.text.Format;
+
+
+public class ArmorMetrial extends ArmorMaterials {
+}
